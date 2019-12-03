a5f8cbf9e2d8be42b9f5f49af370f7a812463914
Ivan <vanya.neytrino@yandex.ru>
2019-12-03 11:10:37 +0300

    clear

--- src/yandex-translator.cr
+++ src/yandex-translator.cr
@@ -4,6 +4,4 @@ module Yandex
   VERSION = "0.1.0"
   raise Exception.new("Some error")
   # TODO: Put your code here
-  y = Yandex::Translator.new("trnsl.1.1.20170418T084954Z.c456230e197006ed.2b34f016e9411a7b745a839ebd19519903bff340")
-  puts y.langs.to_s
 end
